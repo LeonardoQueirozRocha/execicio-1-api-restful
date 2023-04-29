e9f164983d6e9295b4671e5a0c0be13a704d006d
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2023-04-29 15:30:12 -0300

    Ajustes no server.js

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
 require('dotenv').config();
 
-const domain = 'leonardo.vps.webdock.cloud';
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
-const options = {
-    key: fs.readFileSync(`/etc/letsencrypt/live/${domain}/privkey.pem`),
-    cert: fs.readFileSync(`/etc/letsencrypt/live/${domain}/fullchain.pem`)
-};
 const cors = require('cors');
 const path = require('path');
 const apiRouter = require('./api/routes/apiRouter');
@@ -18,4 +11,5 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', apiRouter);
 
-https.createServer(options, app).listen(8000)
+let port = process.env.PORT || 3000
+app.listen (port)
