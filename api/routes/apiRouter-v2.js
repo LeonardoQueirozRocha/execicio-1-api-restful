97fd6d29f9bedef34c8c8076d8342f3588dd1c60
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2023-04-30 22:30:03 -0300

    Desenvolvimento dos endpoints

--- api/routes/apiRouter-v2.js
+++ api/routes/apiRouter-v2.js
@@ -8,11 +8,6 @@ const knex = require('knex')({
     }
 });
 
-const getNextId = () => data.products.reduce((arr, oId) => (arr = arr > oId.id ? arr : oId.id), 0) + 1
-const payloadValidate = (body) => body.description === undefined || body.price === undefined || body.brand === undefined;
-const getProductById = (searched_id) => data.products.find(({ id }) => id === +searched_id);
-const productExists = (description) => data.products.map(product => product.description).includes(description);
-
 apiRouterV2.get(endpoint + 'products', function (req, res) {
     knex.select('*').from('products')
         .then(products => res.status(200).json(products))
@@ -20,84 +15,58 @@ apiRouterV2.get(endpoint + 'products', function (req, res) {
 });
 
 apiRouterV2.get(endpoint + 'products/:id', function (req, res) {
-    // const product = getProductById(req.params.id);
+    knex.select('*').from('products')
+        .where('id', req.params.id)
+        .then(product => {
+            if (product[0] === undefined) {
+                res.status(404).json({ message: 'Produto não encontrado' })
+                return;
+            }
 
-    // if (product === undefined || product === null) {
-    //     res.status(404).json({ message: 'Produto não encontrado' });
-    //     return;
-    // }
-
-    // res.status(200).json(product);
+            res.status(200).json(product)
+        })
+        .catch(err => res.status(400).json({ message: `Erro ao obter o produtos: ${err.message}` }))
 });
 
 apiRouterV2.post(endpoint + 'products', function (req, res) {
     knex('products').insert(req.body, ['id'])
-        .then(products => {
-            let id = products[0].id;
-            res.status(201).json({ message: 'Produto inserido com sucesso.', id });
-        })
-        .catch(err => res.status(400).json({ message: `Erro ao inserir produto: ${err.message}` }))
-
-
-    // if (payloadValidate(req.body)) {
-    //     res.status(400).json({ message: 'Payload inválido' });
-    //     return;
-    // }
-
-    // if (productExists(req.body.description)) {
-    //     res.status(400).json({ message: `O produto ${req.body.description} já está cadastrado` });
-    //     return;
-    // }
-
-    // const product = {
-    //     id: getNextId(),
-    //     description: req.body.description,
-    //     price: req.body.price,
-    //     brand: req.body.brand
-    // };
-
-    // data.products.push(product);
-
-    // res.status(201).json(product);
+        .then(products => res.status(201).json({ message: `Produto ${products[0].id} inserido com sucesso.` }))
+        .catch(err => res.status(400).json({ message: `Erro ao inserir produto: ${err.message}` }));
 });
 
 apiRouterV2.put(endpoint + 'products/:id', function (req, res) {
-    // if (payloadValidate(req.body)) {
-    //     res.status(400).json({ message: 'Payload inválido' });
-    //     return;
-    // }
+    knex('products')
+        .where('id', req.params.id)
+        .update({
+            description: req.body.description,
+            price: req.body.price,
+            brand: req.body.brand
+        }, ['id'])
+        .then(product => {
+            if (product[0] === undefined) {
+                res.status(404).json({ message: 'Produto não encontrado' })
+                return;
+            }
 
-    // const product = getProductById(req.params.id);
-
-    // if (product === undefined || product === null) {
-    //     res.status(404).json({ message: 'Produto não encontrado' });
-    //     return;
-    // }
-
-    // product.description = req.body.description;
-    // product.price = req.body.price;
-    // product.brand = req.body.brand;
-
-    // const index = data.products.findIndex(({ id }) => id === +req.params.id);
-
-    // data.products[index] = product;
-
-    // res.status(200).json(data.products[index]);
+            res.status(200).json({ message: `Produto ${product[0].id} atualizado com sucesso` })
+        })
+        .catch(err => res.status(400).json({ message: `Erro ao atualizar produto: ${err.message}` }));
 });
 
 apiRouterV2.delete(endpoint + 'products/:id', function (req, res) {
-    // const product = getProductById(req.params.id);
+    knex.select('*').from('products')
+        .where('id', req.params.id)
+        .then(product => {
+            if (product[0] === undefined) {
+                res.status(404).json({ message: 'Produto não encontrado' })
+                return;
+            }
 
-    // if (product === undefined || product === null) {
-    //     res.status(404).json({ message: 'Produto não encontrado' });
-    //     return;
-    // }
-
-    // var index = data.products.findIndex(({ id }) => id === +req.params.id);
-
-    // data.products.splice(index, 1);
-
-    // res.status(200).json(product);
+            knex('products').del()
+                .where('id', req.params.id)
+                .then(product => res.status(200).json({ message: `Produto ${req.params.id} deletado com sucesso` }))
+        })
+        .catch(err => res.status(400).json({ message: `Erro ao deletar produto: ${err.message}` }));
 });
 
 module.exports = apiRouterV2;
